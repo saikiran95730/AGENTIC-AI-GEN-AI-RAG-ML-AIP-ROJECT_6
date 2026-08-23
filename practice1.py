e8bece76e40d0be719ca01c8cb4838c223cb5541
saikiran95730 <saikiran95730@gmail.com>
2026-08-23 17:09:38 +0530

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,23 @@
+
+first_name = 'PRAKASH'
+last_name = 'SENAPATI'
+country = 'HYD'
+city = 'TELENGANA'
+age = 40087
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+    'first_name': 'SAI',
+    'last_name': 'KIRAN',
+    'country': 'India',
+    'age' : 250
+}
+print('Firstname: ', first_name)
+print('Firstnamelength: ', len(first_name))
+print('Lastname: ', last_name)
+print
+print('Country: ', country)
+print('Age: ', age)
+
+first_name, last_name, country, age, is_married = 'Sai', 'Kiran', 'India', 250
+print(first_name, last_name, country, age, is_married)
